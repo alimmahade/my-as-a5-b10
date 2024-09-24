e2e3faa85825c87e4d86b02b1b634e57a4c2f5e6
Your Namealimmahade <alim.mahade@gmail.com>
2024-09-24 06:13:48 +0600

    Common Function Added

--- donate.js
+++ donate.js
@@ -1,8 +1,13 @@
+// function getInputValueById(id) {
+//   return parseFloat(document.getElementById(id).value);
+// }
+function getEleById(id) {
+  return document.getElementById(id);
+}
 const donateButton = document.getElementById("donate-btn");
 donateButton.addEventListener("click", function () {
   const donateInputField = document.getElementById("donate-input-field");
-  const donateAmountString = donateInputField.value;
-  const donateAmount = parseFloat(donateAmountString);
+  const donateAmount = parseFloat(donateInputField.value);
   if (donateAmount <= 0 || isNaN(donateAmount)) {
     return alert("invalid Donation amount");
   } else {
@@ -17,14 +22,14 @@ donateButton.addEventListener("click", function () {
 
   const updateTotalDonateAmount = previousTotalDonateAmount + donateAmount;
   previousTotalDonateAmountText.innerText = updateTotalDonateAmount;
-  const myBalanceEle = document.getElementById("my-bal");
+  const myBalanceEle = getEleById("my-bal");
   const myBalanceString = myBalanceEle.innerText;
   const myBalance = parseFloat(myBalanceString);
   const updateMyBalance = myBalance - donateAmount;
   myBalanceEle.innerText = updateMyBalance;
   donateInputField.value = "";
 
-  const donateZone = document.getElementById("donate-zone").innerText;
+  const donateZone = getEleById("donate-zone").innerText;
   const historyItem = document.createElement("div");
 
   historyItem.className = "bg-white p-3 rounded-md border border-indigo-500";
@@ -35,10 +40,10 @@ donateButton.addEventListener("click", function () {
   <p class='text-md text-gray-500'>${new Date()}</p>
   `;
 
-  let historySection = document.getElementById("history-section");
+  let historySection = getEleById("history-section");
   historySection.appendChild(historyItem);
 });
-const donationTab = document.getElementById("donation-tab");
+const donationTab = getEleById("donation-tab");
 donationTab.addEventListener("click", function () {
   donationTab.classList.add("bg-[#B4F461]");
   historyTab.classList.remove("bg-[#B4F461]");
